c092860e86fa9a6e72038f1f892765026c9c3107
Russell Coleman <russellsayshi@gmail.com>
2015-11-26 22:51:06 +0000

    Add QRDetector.java

---
+++ app/src/main/java/com/lasarobotics/qrtester/QRDetector.java
@@ -0,0 +1,48 @@
+package org.lasarobotics.vision.test.detection;
+
+import android.graphics.Bitmap;
+
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.ChecksumException;
+import com.google.zxing.FormatException;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.RGBLuminanceSource;
+import com.google.zxing.Reader;
+import com.google.zxing.Result;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.datamatrix.DataMatrixReader;
+import com.google.zxing.qrcode.QRCodeReader;
+
+/**
+ * Created by Russell on 11/26/2015.
+ */
+public class QRDetector {
+    QRCodeReader qrc;
+
+    public QRDetector() {
+        qrc = new QRCodeReader();
+    }
+
+    public Result detectFromBitmap(Bitmap bMap) throws FormatException, ChecksumException, NotFoundException {
+        //Convert Bitmap into BinaryBitmap
+        int[] intArray = new int[bMap.getWidth()*bMap.getHeight()];
+        //Copy pixel data from the Bitmap into the 'intArray' array
+        bMap.getPixels(intArray, 0, bMap.getWidth(), 0, 0, bMap.getWidth(), bMap.getHeight());
+        LuminanceSource source = new RGBLuminanceSource(bMap.getWidth(), bMap.getHeight(),intArray);
+
+        //Send BinaryBitmap to other method
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+        return detectFromBinaryBitmap(bitmap);
+    }
+
+    public Result detectFromBinaryBitmap(BinaryBitmap map) throws NotFoundException, ChecksumException, FormatException {
+        //Read QR data from BinaryBitmap
+        Reader reader = new DataMatrixReader();
+        return reader.decode(map);
+    }
+
+    public void reset() {
+        qrc.reset();
+    }
+}
